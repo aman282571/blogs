f6205d1e9156698a9603223baade6748d6618c5f
aman282571 <aman282571@gmail.com>
2021-08-03 14:01:41 +0530

    a

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose
   });
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded());
+app.use("/api", require("./routers"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "./client/build")));
   // Step 2:
@@ -27,7 +28,6 @@ if (process.env.NODE_ENV === "production") {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
-app.use("/api", require("./routers"));
 
 app.listen(Port, () => {
   console.log(`listening on port ${Port}`);
